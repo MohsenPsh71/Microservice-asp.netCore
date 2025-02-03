d1763e1490a9f7cfffa5f5c316b108410779997b
mohsen <71mohsenpsh@gmail.com>
2025-02-03 21:07:24 +0330

    Add AutoMapper service to DI container in Program.cs
    Registered AutoMapper with the application's service container
    by adding `builder.Services.AddAutoMapper(typeof(Program).Assembly);`.
    This enables object-to-object mapping throughout the application
    by scanning the current assembly for mapping profiles.

--- Services/Discount/Discount.Grpc/Program.cs
+++ Services/Discount/Discount.Grpc/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
