49d99e11ea85953f5ac775cb24c9d2e2950d14ff
mohsen <71mohsenpsh@gmail.com>
2025-02-05 07:03:28 +0330

    Add infrastructure services and repositories
    Added `Ordering.Infrastructure` namespace to `Program.cs` and registered infrastructure services. Created `InfrastructureServiceRegistration` for service registration, including `OrderContext`, `RepositoryBase<>`, `OrderRepository`, and `EmailService`. Implemented `EmailService` for email operations. Updated `Ordering.Infrastructure.csproj` with necessary package references. Added `OrderRepository` for order-specific operations and `RepositoryBase` for common data access methods.

--- Services/Ordering/Ordering.Api/Program.cs
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -1,4 +1,5 @@
 using Ordering.Application;
+using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddSwaggerGen();
 
 #region Dependencies
 ApplicationServiceRegistration.AddApplicationServices(builder.Services);
+InfrastructureServiceRegistration.AddInfrastructureServices(builder.Services, builder.Configuration);
 #endregion
 
 var app = builder.Build();
