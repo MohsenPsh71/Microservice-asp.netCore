6264e16039064d300ec8d792af6991d62bc0f6ac
mohsen <71mohsenpsh@gmail.com>
2025-02-05 04:58:31 +0330

    Add application services and update dependencies
    - Added `ApplicationServiceRegistration.cs` to define a static class for registering services with DI container.
    - Updated `Program.cs` to include `Ordering.Application` and register services using `AddApplicationServices`.
    - Updated `Ordering.Application.csproj` to include `FluentValidation.DependencyInjectionExtensions` version `11.11.0`.

--- Services/Ordering/Ordering.Api/Program.cs
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -1,3 +1,5 @@
+using Ordering.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Dependencies
+ApplicationServiceRegistration.AddApplicationServices(builder.Services);
+#endregion
 
 var app = builder.Build();
 
