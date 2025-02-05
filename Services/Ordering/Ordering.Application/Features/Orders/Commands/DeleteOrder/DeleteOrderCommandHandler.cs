825b40a1df2e53274a25074321eadc1743ff1788
mohsen <71mohsenpsh@gmail.com>
2025-02-05 04:02:27 +0330

    Add custom exceptions and update command handlers
    Added `NotFoundException` and `ValidationException` classes in the `Ordering.Application.Exceptions` namespace to handle entity not found and validation failures, respectively. Updated `DeleteOrderCommandHandler` and `UpdateOrderCommandHandler` to use `NotFoundException` when the specified order is not found.

--- Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -4,6 +4,8 @@ using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
 {
@@ -27,6 +29,7 @@ namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
             if (orderForDelete == null)
             {
                 _logger.LogError("order not exists");
+                throw new NotFoundException(nameof(Order), request.Id);
                 // other process
             }
             else
